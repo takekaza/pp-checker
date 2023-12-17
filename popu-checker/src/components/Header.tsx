f9900d86213f0d8e65517de7317419e268d725f1
takekaza <takekazaaaaaa@gmail.com>
2023-12-17 16:46:00 +0900

    微調整

--- popu-checker/src/components/Header.tsx
+++ popu-checker/src/components/Header.tsx
@@ -13,8 +13,8 @@ const Header: React.FC = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M26.7407 26.7407L26.7407 0L38 11.2593V38H38H11.2592L-1.14441e-05 26.7407L26.7407 26.7407H26.7407Z"
             fill="black"
           />
